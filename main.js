cdf40dcb108c67ff520c5d9c550c58886c608af2
zital <zital@riseup.net>
2020-11-21 13:56:43 +0100

    readme

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ MongoClient.connect(MongoUrl, function(err, db)
     };
     
     var getGenesisBlock = () => {
-        return new Block(0, "0", getTimestamp(), "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+        return new Block(0, "0", getTimestamp(), "SAFRIE-BLOCKCHAIN", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
     };
     
     var blockchain = [getGenesisBlock()];
