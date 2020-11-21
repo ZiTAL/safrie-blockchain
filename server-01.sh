a1ac6c4558cacc2617fbe70443c7c568234c63af
zital <zital@riseup.net>
2020-11-21 13:22:32 +0100

    first commit

---
+++ server-01.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+COLLECTION=blockchain-01 HTTP_PORT=3001 P2P_PORT=6001 npm start
---
+++ server-02.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+COLLECTION=blockchain-02 HTTP_PORT=3002 P2P_PORT=6002 PEERS=ws://zital-pi.no-ip.org:6001 npm start
